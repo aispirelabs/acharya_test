aa01d42bda48593716ed78536a0b07baeb1e943f
aispirelabs <aispirelabs@gmail.com>
2025-06-19 16:38:43 +0530

    Delete aispirelabs_backend directory

--- aispirelabs_backend/aispirelabs_backend/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('core.authentication.urls')),
-    path('api/acharya-ai/', include('products.acharya_ai.urls')),
-]
